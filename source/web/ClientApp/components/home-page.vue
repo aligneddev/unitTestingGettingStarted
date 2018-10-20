865c8fe3634ea0e588555832a068e1e07630a006
Kevin Logan <kevin.logan@chrsolutions.com>
2018-10-20 16:15:06 -0500

    home page dashboard start

--- source/web/ClientApp/components/home-page.vue
+++ source/web/ClientApp/components/home-page.vue
@@ -1,16 +1,19 @@
 <template>
-    <div>
-      <p> created in the CLI with: `dotnet new -i aspnetcore-vuejs`</p>
-      </div>
+  <div>
+    <h1>Your Biking Statistics</h1>
+    <hr />
+    <h2>Dashboard here - someday</h2>
+    <hr />
+    <p> created in the CLI with: `dotnet new -i aspnetcore-vuejs`</p>
+  </div>
 </template>
-
 <script>
-export default {
-  data () {
-    return {}
+  export default {
+    data () {
+      return {}
+    }
   }
-}
 </script>
-
 <style>
+
 </style>
