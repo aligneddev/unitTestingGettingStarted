274e3fbc85762f5ab46f67746c45ac00e30ff937
Kevin Logan <kevin.logan@chrsolutions.com>
2018-10-20 16:10:07 -0500

    cleaned out default template code, built with component

--- source/web/ClientApp/router/routes.js
+++ source/web/ClientApp/router/routes.js
@@ -1,9 +1,7 @@
-import CounterExample from 'components/counter-example'
-import FetchData from 'components/fetch-data'
 import HomePage from 'components/home-page'
+import BuiltWith from 'components/built-with'
 
 export const routes = [
   { name: 'home', path: '/', component: HomePage, display: 'Home', icon: 'home' },
-  { name: 'counter', path: '/counter', component: CounterExample, display: 'Counter', icon: 'graduation-cap' },
-  { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Fetch data', icon: 'list' }
+  { name: 'builtwith', path: '/builtwith', component: BuiltWith, display: 'Built With Info', icon: 'graduation-cap' },
 ]
