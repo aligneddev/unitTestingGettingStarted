099f509e634178f64467d6e1e3f53b070666a004
Kevin Logan <klogan@omnitech-inc.com>
2020-06-15 10:42:36 -0500

    WeatherStackClientTests to commented out and failing

--- source/API.UnitTests/Weather/WeatherStackClientTests.cs
+++ source/API.UnitTests/Weather/WeatherStackClientTests.cs
@@ -14,97 +14,105 @@ namespace Api.UnitTests.Weather
     [TestCategory(TestCategories.WeatherAPI)]
     public class WeatherStackClientTests
     {
-        private (WeatherStackClient apiuxClient, MockHttpMessageHandler mockHttp) Factory()
+        private (WeatherStackClient weatherStackClient, MockHttpMessageHandler mockHttp) Factory()
         {
             var fakeHttpClient = new Mock<HttpClient>();
 
             // using https://github.com/richardszalay/mockhttp
             var mockHttp = new MockHttpMessageHandler();
             var httpClient = mockHttp.ToHttpClient();
-            var apiuxClient = new WeatherStackClient(httpClient);
-            return (apiuxClient, mockHttp);
+            var weatherStackClient = new WeatherStackClient(httpClient);
+            return (weatherStackClient, mockHttp);
         }
 
         [TestMethod]
         public async Task GetTemp_GivenAZipCode_ReturnsTemp()
         {
-            // Arrange
-            var (apiuxClient, mockHttp) = Factory();
-            const int zipCode = 57105;
-            const long fakeTemp = (long)70.5;
-            var response = new WeatherMainResponse
-            {
-                Current = new WeatherCurrentResponse
-                {
-                    Temperature = fakeTemp
-                }
-            };
+            Assert.Fail();
+            
+            
+            //// Arrange
+            //var (weatherStackClient, mockHttp) = Factory();
+            //const int zipCode = 57105;
+            //const long fakeTemp = (long)70.5;
+            //var response = new WeatherMainResponse
+            //{
+            //    Current = new WeatherCurrentResponse
+            //    {
+            //        Temperature = fakeTemp
+            //    }
+            //};
 
-            var requestUri = $"http://api.weatherstack.com/current?access_key={WeatherStackClient.apiKey}&type=zipcode&units=f&query={zipCode}";
-            mockHttp.When(requestUri)
-                    .Respond("application/json", Serialize.ToJson(response));
+            //var requestUri = $"http://api.weatherstack.com/current?access_key={WeatherStackClient.apiKey}&type=zipcode&units=f&query={zipCode}";
+            //mockHttp.When(requestUri)
+            //        .Respond("application/json", Serialize.ToJson(response));
 
-            // Act
-            var result = await apiuxClient.GetCurrentTempAsync(zipCode);
+            //// Act
+            //var result = await weatherStackClient.GetCurrentTempAsync(zipCode);
 
-            // Assert
-            Assert.AreEqual(fakeTemp, result);
+            //// Assert
+            //Assert.AreEqual(fakeTemp, result);
         }
 
         [TestMethod]
         public async Task GetTemp_GivenAZipCode_UsesThatZipCode()
         {
-            // Arrange
-            var (apiuxClient, mockHttp) = Factory();
-            const int zipCode = 57105;
-            const long fakeTemp = (long)70.5;
-            var response = new WeatherMainResponse
-            {
-                Current = new WeatherCurrentResponse
-                {
-                    Temperature = fakeTemp
-                }
-            };
 
-            var requestUri = $"http://api.weatherstack.com/current?access_key={WeatherStackClient.apiKey}&type=zipcode&units=f&query={zipCode}";
-            var request = mockHttp.When(requestUri)
-                    .Respond("application/json", Serialize.ToJson(response));
+            Assert.Fail();
 
-            // Act
-            await apiuxClient.GetCurrentTempAsync(zipCode);
+            //// Arrange
+            //var (weatherStackClient, mockHttp) = Factory();
+            //const int zipCode = 57105;
+            //const long fakeTemp = (long)70.5;
+            //var response = new WeatherMainResponse
+            //{
+            //    Current = new WeatherCurrentResponse
+            //    {
+            //        Temperature = fakeTemp
+            //    }
+            //};
 
-            // Assert
-            Assert.AreEqual(1, mockHttp.GetMatchCount(request));
+            //var requestUri = $"http://api.weatherstack.com/current?access_key={WeatherStackClient.apiKey}&type=zipcode&units=f&query={zipCode}";
+            //var request = mockHttp.When(requestUri)
+            //        .Respond("application/json", Serialize.ToJson(response));
+
+            //// Act
+            //await weatherStackClient.GetCurrentTempAsync(zipCode);
+
+            //// Assert
+            //Assert.AreEqual(1, mockHttp.GetMatchCount(request));
         }
 
         [TestMethod]
         public async Task GetTemp_GivenAZipCode_NotFound_ThrowsException()
         {
-            // how do we handle error states outside our control?
-            // Arrange
-            var (apiuxClient, mockHttp) = Factory();
-            const int zipCode = 57105;
-            const long fakeTemp = (long)70.5;
-            var response = new WeatherMainResponse
-            {
-                Current = new WeatherCurrentResponse
-                {
-                    Temperature = fakeTemp
-                }
-            };
-            var requestUri = $"http://api.weatherstack.com/current?access_key={WeatherStackClient.apiKey}&type=zipcode&units=f&query={zipCode}";
 
-            var request = mockHttp.When(requestUri)
-                    .Respond(HttpStatusCode.NotFound);
+            Assert.Fail();
+            //    // how do we handle error states outside our control?
+            //    // Arrange
+            //    var (weatherStackClient, mockHttp) = Factory();
+            //    const int zipCode = 57105;
+            //    const long fakeTemp = (long)70.5;
+            //    var response = new WeatherMainResponse
+            //    {
+            //        Current = new WeatherCurrentResponse
+            //        {
+            //            Temperature = fakeTemp
+            //        }
+            //    };
+            //    var requestUri = $"http://api.weatherstack.com/current?access_key={WeatherStackClient.apiKey}&type=zipcode&units=f&query={zipCode}";
 
-            // Act and Assert
-            await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
-            {
-                await apiuxClient.GetCurrentTempAsync(zipCode);
-            });
+            //    var request = mockHttp.When(requestUri)
+            //            .Respond(HttpStatusCode.NotFound);
+
+            //    // Act and Assert
+            //    await Assert.ThrowsExceptionAsync<NotFoundException>(async () =>
+            //    {
+            //        await weatherStackClient.GetCurrentTempAsync(zipCode);
+            //    });
+            //}
+
+
+            // TOD add tests for historical
         }
-
-
-        // TOD add tests for historical
-    }
 }
